bb45bbb85d90fbc54d9f7831ce4fe3dd9b973801
Fernando Sonego <fsonego@algeiba.com>
2019-11-14 15:58:52 -0300

    Blazor Server-Side Demo

---
+++ BlazorAppDemo/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppDemo.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
